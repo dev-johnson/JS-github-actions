907e64ae61aa0117bc3cddc129000361191eb15e
Johnson T <t.johny0325@gmail.com>
2020-11-04 17:20:21 +0530

    Fixed lint issue

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,5 @@
 const { add } = require('../src/index');
 describe('Test index', () => {
-  console.log('should throw eslint error');
   test('add(): a + b = c', () => {
     const a = 5;
     const b = 5;
